3b19322e403e06174d4e3276aa29c6ece7768f28
DiezJotas <jose.jdg37@gmail.com>
2025-07-11 10:52:51 +0000

    Final semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const [encendido, setEncendido ] = useState("amarilla")
+
+	return (
+
+		<div className="semaforo">
+			<div
+			onClick={()=> setEncendido("roja")} 
+			className={"luz roja " + (encendido === "roja" ? "glow" : " ")}></div>
+			<div 
+			onClick={()=> setEncendido("amarilla")} 
+			className={"luz amarilla " + (encendido === "amarilla" ? "glow" : " ")}></div>
+			<div 
+			onClick={()=> setEncendido("verde")} 
+			className={"luz verde " + (encendido === "verde" ? "glow" : " ")}></div>
+
+			<button 
+
+			
+			onClick={()=> {
+              const colores = ["roja","amarilla", "verde"];
+              const colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
+              setEncendido(colorAleatorio);
+			}
+			}
+			className="btn btn-danger"></button>
 		</div>
+
+
+
+
 	);
 };
 
